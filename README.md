182b6196ec01bf0bccc0c72087abb0e0eb134849
espoal <alberto.websci@gmail.com>
2022-03-16 14:08:17 +0100

    feat: smart item

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # 3stat
 [3pass] Analytics solution
+
+
+
+## Install the SDK
+
+Type `yarn add @3pass/3stat`
+
+## Use the SDK
+
+In your scene, from `game.ts` you can register the events with:
+
+`
+import { registerDclEvents } from "../node_modules/@3pass/3stat/index"
+
+\\ Insert your code here
+
+registerDclEvents()
+`
+
+Our SDK will automatically detect the land parcel, so no customerId is needed.
---
+++ items/banner/src/item.ts
@@ -0,0 +1,51 @@
+import { getParcel } from '@decentraland/ParcelIdentity'
+
+const fireEvent = (event: any, msg: any) => log('event!')
+
+
+export default class AnalyticBanner {
+
+    init() {}
+
+    async renderBanner(host: Entity) {
+        // const {parcelId, baseParcel} = await registerDclEvents()
+
+
+        const parcel = await getParcel()
+        const { cid: parcelId } = parcel
+        const baseParcel = parcel.land.sceneJsonData.scene.base
+        log({baseParcel, parcelId})
+
+
+        // const itemId = baseParcel.replace('-','m')
+        // TODO: itemId as parameter
+        const url = `https://europe-west3-backend-339310.cloudfunctions.net/serve/dcl:${baseParcel}.jpg`
+
+        let QRPlane = new Entity()
+        QRPlane.setParent(host)
+        QRPlane.addComponent(new PlaneShape())
+        QRPlane.addComponent(
+            new Transform({
+                position: new Vector3(0, 0.5, 0),
+                rotation: Quaternion.Euler(0, 0, 0),
+                scale: new Vector3(1, 1, 1),
+            })
+        )
+
+        let QRMaterial = new Material()
+
+        QRMaterial.metallic = 0
+        QRMaterial.roughness = 1
+        QRMaterial.specularIntensity = 0
+
+        let QRTexture = new Texture(url)
+        QRMaterial.albedoTexture = QRTexture
+        QRPlane.addComponent(QRMaterial)
+
+    }
+
+    spawn(host: Entity) {
+        log('hello from spawn!!!')
+        this.renderBanner(host)
+    }
+}
